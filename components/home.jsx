7329f13db838ce025d44cc9369a901a50841cc6b
James Muhoro <jmuhoro@innovexsolutions.co.ke>
2023-02-20 08:47:00 +0300

    Set up prettier configuration

---
+++ components/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Footer } from "@/components/sections/footer";
+
+export const Home = () => {
+    return (
+        <div>
+            <p>Hello from Home</p>
+            <Footer />
+        </div>
+    );
+}
